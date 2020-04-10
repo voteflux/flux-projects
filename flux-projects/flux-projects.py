a3372561d7b2b386f90ce3be6014b1fc8469eac8
connorhsm <connorhsmith@gmail.com>
2020-04-10 22:04:10 +1000

    Importing of all cogs on start

--- flux-projects/flux-projects.py
+++ flux-projects/flux-projects.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import config_manager as config
+import os
 
 print(config.check())
 
@@ -10,6 +11,11 @@ flux = commands.Bot(command_prefix=config.read('bot_prefix'), case_insensitive=T
 @flux.event
 async def on_ready():
     print('Flux Projects bot is ready.')
+
+# Loading of all cog files in the cogs directory
+for filename in os.listdir('flux-projects/cogs'):
+    if filename.endswith('.py'):
+        flux.load_extension(f'cogs.{filename[:-3]}')
 
 
 flux.run(config.read('bot_token'))
