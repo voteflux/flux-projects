fc0eaa10eaa0681d4701eefd792e95b67960cdf0
connorhsm <connorhsmith@gmail.com>
2020-04-19 00:42:02 +1000

    Support objective data configuration.

--- flux-projects/config_manager.py
+++ flux-projects/config_manager.py
@@ -1,17 +1,29 @@
 import configparser
 import os.path
+from ast import literal_eval
 
 def check():
     if os.path.isfile('flux-projects/config.ini'):
         return('Using existing config.')
     else:
         config = configparser.ConfigParser()
+
         config['Settings'] = {'bot_token': 'token',
                               'bot_prefix': '!',
                               'db_host': 'localhost',
                               'db_db': 'database',
                               'db_user': 'username',
                               'db_pass': 'password'}
+
+        config['Objectives'] = {'0': ('Awareness & Outreach', 'red'),
+                                '1': ('Party Operation', 'gold'),
+                                '2': ('Fundraising', 'magenta'),
+                                '3': ('Campaigning (tech)', 'purple'),
+                                '4': ('Campaigning (elections)', 'orange'),
+                                '5': ('Development', 'dark_blue'),
+                                '6 ': ('Resource & Data Creation', 'green'),
+                                '7': ('Scaling Party Membership', 'blue')}
+
         with open('flux-projects/config.ini', 'w') as f:
             config.write(f)
         return('Config file does not exist. Created with default values.')
@@ -20,6 +32,11 @@ def read(setting):
     config = configparser.ConfigParser()
     config.read('flux-projects/config.ini')
     return(config['Settings'][setting])
+
+def objective_data(objective):
+    config = configparser.ConfigParser()
+    config.read('flux-projects/config.ini')
+    return(literal_eval(config['Objectives'][objective]))
 
 def db_config():
     db_config = {'host': str(read('DB_host')),
