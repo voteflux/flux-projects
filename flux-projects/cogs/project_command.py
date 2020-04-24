b36a69cfea92b1b09635639ab40b8229e7975a5a
connorhsm <connorhsmith@gmail.com>
2020-04-24 13:11:54 +1000

    Convert db connections to use new connection handler

--- flux-projects/cogs/project_command.py
+++ flux-projects/cogs/project_command.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import mysql.connector
 import utility.config_manager as config
 from discord.ext.commands.cooldowns import BucketType
+from utility.db_manager import db_connection
 
 
 class Project_Command(commands.Cog):
@@ -34,69 +35,43 @@ class Project_Command(commands.Cog):
             id = await self.get_latest_project_ID()
             content = "This is the latest project:"
 
-        try:
-            db = mysql.connector.connect(**config.db_config())
+        with db_connection() as db:
+            db.execute(f'SELECT * FROM `projects` WHERE `id` = \'{id}\' LIMIT 1')
+            project = db.fetchone()
 
-            if db.is_connected:
-                cursor = db.cursor()
-                cursor.execute(f'SELECT * FROM `projects` WHERE `id` = \'{id}\' LIMIT 1')
+        if project is None:
+            await ctx.message.delete()
+            return await ctx.send(f'{ctx.author.mention}, There are no projects by the ID `{id}`.', delete_after=10)
 
-        except mysql.connector.Error as e:
-            raise e
+        author = ctx.guild.get_member(project[8])
 
-        else:
-            project = cursor.fetchone()
+        # API request for a User object instead of a Member object incase the user is no longer in the guild
+        if author is None:
+            author = await self.flux.fetch_user(project[8])
 
-            if project is None:
-                await ctx.message.delete()
-                return await ctx.send(f'{ctx.author.mention}, There are no projects by the ID `{id}`.', delete_after=10)
+        colour = await commands.ColourConverter.convert(self, ctx, config.objective_data(str(project[7]))[1])
 
-            author = ctx.guild.get_member(project[8])
+        embed = discord.Embed(title=project[1], description=project[4], color=colour)
+        embed.set_author(name=author.display_name, icon_url=author.avatar_url)
+        
+        # Unnecessary information. Can be used later on for in depth message about project.
+        
+        # This field is a blank line break for inline field organising purposes
+        #embed.add_field(name='\u200b', value='\u200b', inline=False)
+        #embed.add_field(name='Resouces', value=project[9], inline=True)
+        #embed.add_field(name='Outcomes', value=project[5], inline=True)
+        #embed.add_field(name='Deliverables', value=project[6], inline=True)
+        embed.add_field(name='Objective', value=config.objective_data(str(project[7]))[0], inline=True)
+        embed.add_field(name='Completion', value=project[3], inline=True)
+        embed.add_field(name='Status', value=project[11], inline=True)
+        embed.set_footer(text=f'Project ID #{project[0]}  |  Flux {"Official" if project[10] else "Volunteer"} Project')
 
-            # API request for a User object instead of a Member object incase the user is no longer in the guild
-            if author is None:
-                author = await self.flux.fetch_user(project[8])
-            
-            cursor.close()
-            db.close()
-
-            colour = await commands.ColourConverter.convert(self, ctx, config.objective_data(str(project[7]))[1])
-
-            embed = discord.Embed(title=project[1], description=project[4], color=colour)
-            embed.set_author(name=author.display_name, icon_url=author.avatar_url)
-            
-            # Unnecessary information. Can be used later on for in depth message about project.
-            
-            # This field is a blank line break for inline field organising purposes
-            #embed.add_field(name='\u200b', value='\u200b', inline=False)
-            #embed.add_field(name='Resouces', value=project[9], inline=True)
-            #embed.add_field(name='Outcomes', value=project[5], inline=True)
-            #embed.add_field(name='Deliverables', value=project[6], inline=True)
-            embed.add_field(name='Objective', value=config.objective_data(str(project[7]))[0], inline=True)
-            embed.add_field(name='Completion', value=project[3], inline=True)
-            embed.add_field(name='Status', value=project[11], inline=True)
-            embed.set_footer(text=f'Project ID #{project[0]}  |  Flux {"Official" if project[10] else "Volunteer"} Project')
-
-            await ctx.send(content=content, embed=embed)
+        await ctx.send(content=content, embed=embed)
 
     async def get_latest_project_ID(self):
-        
-        try:
-            db = mysql.connector.connect(**config.db_config())
-
-            if db.is_connected:
-                cursor = db.cursor()
-                cursor.execute('SELECT id FROM `projects` ORDER BY `projects`.`id` DESC LIMIT 1')
-
-        except mysql.connector.Error as e:
-            raise e
-
-        else:
-            project_ID = cursor.fetchone()
-            
-            cursor.close()
-            db.close()
-
+        with db_connection() as db:
+            db.execute('SELECT id FROM `projects` ORDER BY `projects`.`id` DESC LIMIT 1')
+            project_ID = db.fetchone()
             return project_ID[0]
 
     @project.command(brief='Create a new project.', help='Creates a new project. The bot will message you questions to complete the required information.')
