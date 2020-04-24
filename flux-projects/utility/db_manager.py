016bd63c04c91ccbdd1b32b5d24333317f37a2b1
connorhsm <connorhsmith@gmail.com>
2020-04-24 13:03:47 +1000

    Created a db manager

---
+++ flux-projects/utility/db_manager.py
@@ -0,0 +1,17 @@
+from utility.config_manager import db_config
+import mysql.connector
+
+
+class db_connection:
+    
+    def __init__(self):
+        self.db = mysql.connector.connect(**db_config())
+        self.cursor = self.db.cursor()
+
+    def __enter__(self):
+            return self.cursor
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.cursor:
+            self.cursor.close()
+            self.db.close()
