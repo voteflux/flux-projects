9578883e1428744011c31ff3f9553bd1ce725d69
connorhsm <connorhsmith@gmail.com>
2020-04-26 12:13:33 +1000

    Added missing import and corrected formatting of New command

--- flux-projects/cogs/new.py
+++ flux-projects/cogs/new.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord.ext.commands.cooldowns import BucketType
 
 
 class New(commands.Cog):
@@ -7,13 +8,11 @@ class New(commands.Cog):
     def __init__(self, flux):
         self.flux = flux
 
-
     @commands.command(brief='Create a new project.', help='Creates a new project. The bot will message you questions to complete the required information.')
     @commands.max_concurrency(1, per=BucketType.user, wait=False)
     async def new(self, ctx):
         await ctx.message.delete()
         await ctx.send(f'{ctx.author.mention}, To create a new project, please answer the questions I send to you in a private message.', delete_after=10)
-
 
 
 def setup(flux):
