e42da52b26a1d528bfb2a514a5d4525b67a1c65b
connorhsm <connorhsmith@gmail.com>
2020-04-29 18:40:55 +1000

    Updated data structure, issues caused by Python interpreting int 0 as false.

--- flux-projects/utility/config_manager.py
+++ flux-projects/utility/config_manager.py
@@ -16,27 +16,27 @@ def check():
                               'user': 'username',
                               'pass': 'password'}
 
-        config['Objectives'] = {'0': ('Awareness & Outreach', 'red'),
-                                '1': ('Party Operation', 'gold'),
+        config['Objectives'] = {'1': ('Party Operation', 'gold'),
                                 '2': ('Fundraising', 'magenta'),
                                 '3': ('Campaigning (tech)', 'purple'),
                                 '4': ('Campaigning (elections)', 'orange'),
                                 '5': ('Development', 'dark_blue'),
                                 '6 ': ('Resource & Data Creation', 'green'),
-                                '7': ('Scaling Party Membership', 'blue')}
+                                '7': ('Scaling Party Membership', 'blue'),
+                                '8': ('Awareness & Outreach', 'red')}
 
-        config['Resources'] = {'0': ('Funding', 402823346854559744),
-                                '1': ('Media Accounts Access', 313952769541406720),
+        config['Resources'] = {'1': ('Media Accounts Access', 313952769541406720),
                                 '2': ('Official Account Access', 140741468787572736),
                                 '3': ('Pull Request Review(App)', 449910203220099073),
                                 '4': ('Pull Request Review (VoteFlux)', 199731686093619200),
-                                '5': ('Membership Info', 402823346854559744)}
+                                '5': ('Membership Info', 402823346854559744),
+                                '6': ('Funding', 402823346854559744)}
 
-        config['Status'] = {'0': 'Active',
-                            '1': 'Open',
+        config['Status'] = {'1': 'Open',
                             '2': 'Done',
                             '3': 'Blocked',
-                            '4': 'Out Of Scope'}
+                            '4': 'Out Of Scope',
+                            '5': 'Active'}
 
         with open('flux-projects/utility/config.ini', 'w') as f:
             config.write(f)
