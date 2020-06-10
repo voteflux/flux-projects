84c8b41f0bb47b8006c4debd49fb7f355b2301f7
connorhsm <--global>
2020-06-10 21:03:53 +1000

    Beginning structure for new issue command

---
+++ flux-projects/cogs/issue.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands.cooldowns import BucketType
+from discord.utils import get
+
+
+class Issue(commands.Cog):
+
+    def __init__(self, flux):
+        self.flux = flux
+        self.question = self.flux.get_cog('Question')
+
+    @commands.command(brief='Create a new issue for DigiPol', help='Create a new issue for the app. You will be prompted to answer some questions by the bot.')
+    @commands.max_concurrency(1, per=BucketType.user, wait=False)
+    @commands.has_role('Flux Vetted')
+    async def issue(self, ctx):
+        await ctx.message.delete()
+
+        embed = discord.Embed(description='You\'re now creating a new issue for the app, here is what I need to know from you:', colour=discord.Colour.green())
+        await ctx.author.send(embed=embed)
+
+
+def setup(flux):
+    flux.add_cog(Issue(flux))
